acf6cdcb0ab4ee92ccd5bbd3418e7b5023c88b19
sameerbd <sam.saqui@gmail.com>
2025-01-25 23:14:09 +0600

    Update db.php

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
 $host = 'localhost';
 $user = 'root'; // Replace with your DB username
-$pass = 'roottesting2025'; // Replace with your DB password
+$pass = ''; // Replace with your DB password
 $dbname = 'photo_gallery';
 
 $conn = new mysqli($host, $user, $pass, $dbname);
